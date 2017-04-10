2db6ba17ed3915d07e8515a836c2342f1d791180
ioshero <handsomeman1011@gmail.com>
2017-04-10 03:37:38 +0200

    init commit

---
+++ src/main/java/com/probojnik/testDrivenDevelopment/Expression.java
@@ -0,0 +1,10 @@
+package com.probojnik.testDrivenDevelopment;
+
+/**
+ * @author ioshero
+ */
+public interface Expression {
+    public Money reduce(Bank bank, String to);
+    public Expression plus(Expression addend);
+    public Expression times(int multiplier);
+}
